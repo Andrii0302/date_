a0dc8f6ec5b0816c633576fe84080c880e050ed7
Andrii Diduk <144677052+Andrii0302@users.noreply.github.com>
2024-10-07 20:03:55 +0300

    Update forms.py

--- meet/forms.py
+++ meet/forms.py
@@ -4,8 +4,9 @@ from .models import DateTable
 class DateForm(forms.ModelForm):
     class Meta:
         model = DateTable
-        fields = ['date','time']
+        fields = ['date','time','name']
         labels = {
             'date': 'Pick a date (2024-12-01)',
             'time': 'Pick a time (00:00:00)',
+            'name':'Add a comment if you want to'
         }
